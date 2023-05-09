ae882bc628c6672755386139105ef89163594659
DreamingCodes <dev@dreaming.codes>
2023-05-10 00:29:38 +0200

    Use parking_lot for mutex and fixed pads

--- src/discordstreamer/imp.rs
+++ src/discordstreamer/imp.rs
@@ -1,7 +1,8 @@
-use std::sync::Mutex;
-use gst::glib;
+use gst::{Caps, glib, info, Pad, PadTemplate};
+use gst::prelude::*;
 use gst::subclass::prelude::*;
 use once_cell::sync::Lazy;
+use parking_lot::Mutex;
 use tokio::runtime::Handle;
 
 pub static CAT: Lazy<gst::DebugCategory> = Lazy::new(|| {
@@ -12,14 +13,14 @@ pub static CAT: Lazy<gst::DebugCategory> = Lazy::new(|| {
     )
 });
 
-#[derive(Default)]
 struct State {
     handle: Option<Handle>,
+    video_sink: Pad,
+    audio_sink: Option<Pad>,
 }
 
-#[derive(Default)]
 pub struct DiscordStreamer {
-    state: Mutex<State>
+    state: Mutex<State>,
 }
 
 impl DiscordStreamer {
@@ -27,11 +28,11 @@ impl DiscordStreamer {
         &self,
         handle: Handle
     ) {
-        let _ = self.state.lock().unwrap().handle.insert(handle);
+        let _ = self.state.lock().handle.insert(handle);
     }
 
     fn runtime_handle(&self) -> Handle {
-        self.state.lock().unwrap().handle.as_ref().unwrap_or(crate::RUNTIME.handle()).clone()
+        self.state.lock().handle.as_ref().unwrap_or(crate::RUNTIME.handle()).clone()
     }
 }
 
@@ -42,14 +43,26 @@ impl ObjectSubclass for DiscordStreamer {
     type ParentType = gst::Element;
 
     fn with_class(klass: &Self::Class) -> Self {
+        let templ = klass.pad_template("video_sink").unwrap();
+        let video_sink = Pad::builder_with_template(&templ, Some("video_sink")).build();
+
         Self {
-            state: Mutex::new(Default::default())
+            state: Mutex::new(State{
+                handle: None,
+                video_sink,
+                audio_sink: None,
+            }),
         }
     }
 
 }
+impl ObjectImpl for DiscordStreamer {
+    fn constructed(&self) {
+        self.parent_constructed();
 
-impl ObjectImpl for DiscordStreamer {}
+        self.obj().add_pad(&self.state.lock().video_sink).unwrap();
+    }
+}
 
 impl GstObjectImpl for DiscordStreamer {}
 
@@ -65,5 +78,39 @@ impl ElementImpl for DiscordStreamer {
         });
 
         Some(&*ELEMENT_METADATA)
+    }
+
+    fn pad_templates() -> &'static [PadTemplate] {
+        static PAD_TEMPLATES: Lazy<Vec<PadTemplate>> = Lazy::new(|| {
+            let video_sink_pad_template = PadTemplate::new(
+                "video_sink",
+                gst::PadDirection::Sink,
+                gst::PadPresence::Always,
+                &Caps::new_empty_simple("video/x-raw"),
+            ).unwrap();
+
+            let audio_sink_pad_template = PadTemplate::new(
+                "audio_sink",
+                gst::PadDirection::Sink,
+                gst::PadPresence::Request,
+                &Caps::new_empty_simple("audio/x-raw"),
+            ).unwrap();
+
+            vec![video_sink_pad_template, audio_sink_pad_template]
+        });
+
+        PAD_TEMPLATES.as_ref()
+    }
+
+    //TODO: Implement audio pad sink request
+    fn request_new_pad(&self, templ: &PadTemplate, name: Option<&str>, caps: Option<&Caps>) -> Option<Pad> {
+        if templ.name_template() == "audio_sink" {
+            let audio_sink = Pad::builder_with_template(templ, name).build();
+            self.obj().add_pad(&audio_sink).unwrap();
+            self.state.lock().audio_sink = Some(audio_sink.clone());
+            return Some(audio_sink);
+        }
+
+        None
     }
 }
